f94c6a8bcf0c99cc0cbc05872894b48985f0631c
Quixote-Shanks <58040220+Quixote-Shanks@users.noreply.github.com>
2023-02-11 00:35:55 +0100

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -3,4 +3,4 @@ const app = require('./app');
 
 // app.listen( process.env.PORT || 7070, () => console.log('Listen on port ' + process.env.PORT || 7070) );
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 7070)
